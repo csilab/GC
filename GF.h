1e58cb6e782ff911f24a1f0f4618d0654e4bce25
HieuNguyen202 <edwardnguyen202@gmail.com>
2017-08-17 13:00:53 -0500

    Added comments for GF and Decoder classes.

--- GF.h
+++ GF.h
@@ -14,9 +14,36 @@ public:
 	long gfdiv(long den, long num);
 	void toString();
 private:
+    /**
+    Check if the input number is a prime number.
+
+    @param num The input number.
+
+    @return whether the input number is  a prime number.
+    */
 	bool isPrime(long num);
+    /**
+    Find the smallest prime number that is greater than the input number.
+
+    @param num The input number.
+
+    @return The next prime number.
+    */
 	long nextPrime(long num);
+    /**
+    Find the smallest prime number that is greater than the input number.
+
+    @param num The input number.
+
+    @return The next prime number.
+    */
 	void setDem(int lengthExtension);
+    /**
+     The input sequence will be divided  smaller blocks of sequences. This function calculates
+     the block length and the number of blocks needed to contain the decoding sequence.
+
+    @param lengthExtension blockLength = lengthExtension * log (mlen) .
+    */
 };
 
 #endif // GF_H
